bf7e3bbad691a7c7ed2175554f24abd5bd531f65
Paul Gierz <pgierz@awi.de>
2024-05-23 09:44:06 +0200

    docs(version.rb): yard via ChatGPT

--- lib/version.rb
+++ lib/version.rb
@@ -1,3 +1,7 @@
 module Seamore
-  VERSION = '2.1.1' # version number according to semantic versioning (SemVer) paradigm
+  # The current version of the Seamore library following the semantic 
+  # versioning (SemVer) paradigm.
+  #
+  # @return [String] the current version of the Seamore library.
+  VERSION = '2.1.1'
 end
